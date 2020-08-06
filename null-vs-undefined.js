0b14cf8dcaed73b9ebfcf5c5bc86d273d3921992
Shoyas <md.nasiruddin4067@gmail.com>
2020-08-07 02:19:26 +0600

    null Vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,15 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2){
+    console.log(num1 + num2);
+}
+const result = add(12, 12);
+console.log(result);
+
+const premik = { name: "smart boy", phone: 93293284};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
